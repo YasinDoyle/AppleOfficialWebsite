47b50536c6969579ab78a92b73ac3204d1c7de05
YasinDoyle <yasindoyle97@gmail.com>
2025-05-13 23:43:36 +0800

    nav结构

--- AppleOfficialWebsite/src/App.vue
+++ AppleOfficialWebsite/src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="globalheader">
+    <!--aside不明确-->
     <aside></aside>
+    <!--导航栏-->
     <nav id="globalnav">
-      <div class="globalnav-content"></div>
+      <div class="globalnav-content">
+        <div class="globalnav-item globalnav-menuback"></div>
+        <ul id="globalnav-list" class="globalnav-list">
+          <li class="globalnav-item globalnav-item-apple"></li>
+          <li class="globalnav-item globalnav-menu"></li>
+          <li class="globalnav-item globalnav-search shift-0-1"></li>
+          <li class="globalnav-item globalnav-bag"></li>
+        </ul>
+        <div class="globalnav-menutrigger"></div>
+      </div>
+
     </nav>
+    <!--curtain意义不明-->
+    <div id="globalnav-curtain"></div>
+    <!--placeholder unknown-->
+    <div id="globalnav-placeholder"></div>
   </div>
   <main>
     <section>section1</section>
