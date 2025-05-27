0ec65784464a456b24dc47bb3d51451f2423b5c0
YasinDoyle <yasindoyle97@gmail.com>
2025-05-27 17:56:42 +0800

    添加ProductIntroductionItem组件，包含按钮和图片展示功能

---
+++ AppleOfficialWebsite/src/component/ProductIntroductionItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <!--button-container根据属性改变位置-->
+    <div class="button-container">
+      <button
+        class="description-button"
+        @click="navigateTo(props.descriptionLink)"
+      >
+        {{ props.descriptionText }}
+      </button>
+      <button class="shop-now-button" @click="navigateTo(props.shopNowLink)">
+        {{ props.shopNowText }}
+      </button>
+      <img
+        :src="props.productImgSrc"
+        alt="product-image"
+        class="product-image"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, withDefaults } from "vue";
+import { useRouter } from "vue-router";
+interface Props {
+  descriptionText: string;
+  shopNowText: string;
+  productImgSrc: string;
+  buttonPosition?: "top" | "bottom";
+  descriptionLink?: string;
+  shopNowLink?: string;
+  showShopNow?: boolean;
+  showDescription?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  buttonPosition: "top",
+  descriptionText: "",
+  shopNowText: "",
+  showShopNow: true,
+  showDescription: true,
+});
+
+const router = useRouter();
+
+const navigateTo = (url: string) => {
+  if (!url) return;
+  if (url.startsWith("http//") || url.startsWith("https://")) {
+    window.open(url, "_blank");
+  } else {
+    router.push(url);
+  }
+};
+</script>
+
+<style></style>
